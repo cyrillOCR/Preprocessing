a1d3864e3e606bfea4515267766b41fbd498e4be
Covaliu Lucian <luciancovaliu41@gmail.com>
2018-12-12 22:59:47 +0200

    To boxes updated

--- src/toBoxes.py
+++ src/toBoxes.py
@@ -1,48 +1,143 @@
-import argparse
-import csv
+import sys
+from sys import argv
 
 import cv2
-import pytesseract
+from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
-# ^ change this if installed on another path
+width = 0
+height = 0
+maxW = 0
+minW = sys.maxsize
+maxH = 0
+minH = sys.maxsize
+
+result = list()
 
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-                help="path to input image to be parsed")
-args = vars(ap.parse_args())
+def debug(x):
+    global img
+    cv2.rectangle(img, (x[0], x[1]), (x[2], x[3]), (255, 0, 0), 2)
 
-img = cv2.imread(args["image"])
 
-# img = cv2.imread(filename)
-h, w, _ = img.shape
-text = pytesseract.image_to_boxes(img, lang="rus").encode("utf-8")
+def resetHW():
+    global maxW
+    global maxH
+    global minH
+    global minW
+    maxW = 0
+    minW = sys.maxsize
+    maxH = 0
+    minH = sys.maxsize
 
-file = open("boxes4.txt", mode="wb")
-file.write(text)
-file.close()
 
-boxes = []
-coords = []
-with open('boxes4.txt', 'r', encoding="utf-8") as f:
-    reader = csv.reader(f, delimiter=' ')
-    for row in reader:
-        if len(row) == 6:
-            boxes.append(row)
+def inMatrix(x, y):
+    global width
+    global height
+    if y < 0 or x < 0 or y > width or x > height:
+        return False
+    return True
 
-for b in boxes:
-    img = cv2.rectangle(img, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (255, 0, 0), 2)
-    coords.append((int(b[1]), h - int(b[2]), int(b[3]), h - int(b[4])))
 
-print(coords)
+def flood(x, y):
+    global pixels
+    global maxW
+    global maxH
+    global minH
+    global minW
+    if not inMatrix(x, y):
+        return
+    if pixels[x][y] == 1:
+        pixels[x][y] = 2
+        if y > maxW:
+            maxW = y
+        if x > maxH:
+            maxH = x
+        if y < minW:
+            minW = y
+        if x < minH:
+            minH = x
+        flood(x + 1, y)
+        flood(x + 1, y + 1)
+        flood(x + 1, y - 1)
+        flood(x - 1, y)
+        flood(x - 1, y + 1)
+        flood(x - 1, y - 1)
+        flood(x, y - 1)
+        flood(x, y + 1)
 
-file = open("output4.txt", mode="w+")
-file.write('\n'.join(str(e) for e in coords))
-file.close()
 
-cv2.imwrite("output4.png", img)
+def GetPixels(input_path):
+    global width
+    global height
+    global pixels
 
-# cv2.imshow('output', img)
+    inp = Image.open(input_path)
+    imgPixels = inp.load()
+    width, height = inp.size
+    pixels = [[0] * width for i in range(height)]
 
-cv2.waitKey(0)
+    for i in range(width):
+        for j in range(height):
+            if imgPixels[i, j][0] == 0:
+                pixels[j][i] = 1
+
+
+def isCompletlyIn(x, y):
+    return x[0] < y[0] and x[1] < y[1] and x[2] > y[2] and x[3] > y[3]
+
+
+def removeRedundant():
+    toRemove = list()
+    for i in result:
+        for j in result:
+            if isCompletlyIn(i, j):
+                toRemove.append(j)
+
+    for i in toRemove:
+        result.remove(i)
+
+
+def write(file):
+    for r in result:
+        debug(r)
+        file.write(f"({r[0]}, {r[1]})\n({r[2]}, {r[3]})\n")
+
+
+def fullFlood(lines, output_path):
+    file = open(output_path, "w+")
+    for line in lines:
+        for i in range(width):
+            for j in range(line[0], line[1]):
+                flood(j, i)
+                if minH < sys.maxsize:
+                    result.append((minW, minH, maxW, maxH))
+                    resetHW()
+    removeRedundant()
+    write(file)
+
+
+if __name__ == '__main__':
+    if len(argv) != 4:
+        print("Usage: [].py inputFile inputLines outputFile")
+        exit(0)
+
+    sys.setrecursionlimit(sys.maxsize)  # what do you mean CPU? and RAM?! pfff
+
+    input_path = argv[1]
+    lines_path = argv[2]
+    output_path = argv[3]
+    GetPixels(input_path)
+
+    file = open(lines_path)
+    lines = file.readlines()
+    coordLines = list()
+    for line in range(0, len(lines), 2):
+        coordLines.append((int(lines[line]), int(lines[line + 1])))
+
+    # debug
+    img = cv2.imread(input_path)
+
+    fullFlood(coordLines, output_path)
+
+    # debug
+    cv2.imwrite("output_boxes.png", img)
