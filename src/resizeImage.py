12d1db8efa0720be9829e62c17b2a2f37b8e10ac
malinac <malina.catanoiu@gmail.com>
2018-12-28 21:31:50 +0200

    updated resizeImage

--- src/resizeImage.py
+++ src/resizeImage.py
@@ -1,29 +1,36 @@
 from PIL import Image
-import sys
-def resizeImg(input_image_path, output_image__path, maxSize, properSize):
-    input_image_file = Image.open(input_image_path)
-    width, height = input_image_file.size
+from sys import argv
+def resizeImg(image, maxSize, properSize):
+    width, height = image.size
     percentage = (int(properSize)*100)/int(maxSize)
     percentage = int(percentage)/100
     if width >= int(maxSize):
         percentage = (int(properSize)) / int(width)
         hsize = int((float(height))*float(percentage))
-        input_image_file.resize((int(properSize),hsize),Image.ANTIALIAS).save(output_image__path)
+        image = image.resize((int(properSize),hsize),Image.ANTIALIAS)
     if height >= int(maxSize):
         percentage = (int(properSize))/ int(height)
         wsize = int((float(width)) * float(percentage))
-        input_image_file.resize((wsize, int(properSize)), Image.ANTIALIAS).save(output_image__path)
+        image = image.resize((wsize, int(properSize)), Image.ANTIALIAS)
+    return image
+
 
 
 if __name__== '__main__':
-    if len(sys.argv) > 5 or len(sys.argv) < 4:
-        print("Invalid number of arguments. Usage: resizeImage.py input-image-path output-image-path maxSize properSize")
+    if len(argv)!=4:
+        print("Invalid number of arguments. Usage: resizeImage.py image_path maxSize properSize")
         exit(-1)
-    file_path = sys.argv[1]
-    file_path_save = sys.argv[2]
-    maxSize = sys.argv[3]
-    properSize = sys.argv[4]
-    resizeImg(file_path, file_path_save, maxSize, properSize)
+    image_path = argv[1]
+    maxSize = argv[2]
+    properSize = argv[3]
+    image = Image.open(image_path)
+    image = resizeImg(image, maxSize, properSize)
+    image.show()
+
+
+
+
+
 
 
 
